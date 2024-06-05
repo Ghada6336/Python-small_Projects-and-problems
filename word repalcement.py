3301857d6c6eea0301f033a518ab62d16fe240ef
Ghada Fahmie <60541992+Ghada6336@users.noreply.github.com>
2024-06-06 01:23:37 +0300

    Add files via upload

---
+++ word repalcement.py
@@ -0,0 +1,8 @@
+def replace_word():
+    main_text = "hello , I am Ghada nice to meet you , have nice day"
+    what_to_replace = input("enter the word to replace :")
+    replacement_word = input("enter the replacement word :")
+    print(main_text.replace(what_to_replace, replacement_word))
+
+
+replace_word()
