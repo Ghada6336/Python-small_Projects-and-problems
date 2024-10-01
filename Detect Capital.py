7f1d24e88dd8f5f6328ce126e79ccba2ca095a3c
Ghada Fahmie <60541992+Ghada6336@users.noreply.github.com>
2024-10-01 23:45:00 +0300

    Add files via upload

---
+++ Detect Capital.py
@@ -0,0 +1,13 @@
+
+def detectCapitalUse(word):
+    if word.isupper():
+        return True
+    elif word.islower():
+        return True
+    elif word.istitle():
+        return True
+    else:
+        return False
+
+
+print(detectCapitalUse("HHHH"))
